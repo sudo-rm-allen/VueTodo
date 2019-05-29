7d8835638fc7962a310fdfb50b60e65957bd219d
Allen Chen <ac11378@hotmail.com>
2019-05-30 07:31:57 +0800

    second

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the Todo List app v1.0.0. it is part of the vue crash
+      course on youtube
+    </p>
+  </div>
+</template>
